0c47e0e3145be16c105234c53361d1f8488cb752
Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
2025-08-22 10:11:36 +0200

    Set default values for rate and duration in k6 script

--- src/expb/payloads/k6_script.py
+++ src/expb/payloads/k6_script.py
@@ -6,8 +6,8 @@ def build_k6_script_config(
     scenario_name: str,
     client: Client,
     iterations: int,
-    rate: Optional[int] = None,           # iterations per second (IPS)
-    duration: Optional[str] = None,       # e.g. "20m", "600s"; if None we'll compute
+    rate: Optional[int] = 2,           # iterations per second (IPS)
+    duration: Optional[str] = "60m",       # e.g. "20m", "600s"; if None we'll compute
     pre_allocated_vus: int = 1,           # >1 enables overlap
     max_vus: int = 1,
     time_unit: str = "1s",
